bb631e4b3840a052611a9029ac45e8036bc85ce8
Álvaro López García <alvarolopez769@gmail.com>
2023-12-18 15:51:41 +0100

    Changes on rewarding system

--- src/simulations/domain/services/reward_service.py
+++ src/simulations/domain/services/reward_service.py
@@ -17,15 +17,13 @@ class RewardService:
         reward = 0
 
         # Reinforcing the robot to get closer
-        if next_state.is_ball_in_sight():
+        if next_state.area_norm > curr_state.area_norm:
             reward += 1
-            if curr_state.area_norm > next_state.area_norm:
-                reward += 1
 
         # Reinforcing the robot to keep the ball centered
-        if abs(next_state.x_norm - 0.5) > 0.15:  # 0.5 +- 15% of deviation
+        if abs(next_state.x_norm - 0.5) >= 0.15:  # 0.5 +- 15% of deviation
             reward += 1
-            if abs(curr_state.x_norm - 0.5) > abs(next_state.x_norm - 0.5):
+            if abs(curr_state.x_norm - 0.5) >= abs(next_state.x_norm - 0.5):
                 reward += 1
 
         return reward
